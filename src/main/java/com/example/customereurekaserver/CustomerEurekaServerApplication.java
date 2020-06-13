cbcff3627e8ce54abafdcd9109a8f8c2cad4f08f
atikurrehman <atikurrehmank@gmail.com>
2020-06-13 19:46:39 +0530

    initilisinz eureka server

---
+++ src/main/java/com/example/customereurekaserver/CustomerEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.customereurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerEurekaServerApplication.class, args);
+	}
+
+}
